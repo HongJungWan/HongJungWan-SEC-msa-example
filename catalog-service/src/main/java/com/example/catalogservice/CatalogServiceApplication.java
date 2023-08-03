22a473ca139342bfde3270e79c6b3abb57f2097a
HongJungWan <hjw43ok@hs.ac.kr>
2023-08-03 15:38:23 +0900

    style(catalog-service): 불필요한 클래스, 파일, 주석 제거

--- catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class CatalogServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
-
 }
--- catalog-service/src/main/java/com/example/catalogservice/service/CatalogServiceImpl.java
+++ catalog-service/src/main/java/com/example/catalogservice/service/CatalogServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class CatalogServiceImpl implements CatalogService{
+public class CatalogServiceImpl implements CatalogService {
 
     private final CatalogRepository catalogRepository;
 
