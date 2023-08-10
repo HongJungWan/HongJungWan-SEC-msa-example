42e617be500a381e53964de06b92d0a0adb84c1b
HongJungWan <hjw43ok@hs.ac.kr>
2023-08-10 16:16:22 +0900

    feat(catalog-service): 전역 예외 처리 #1

---
+++ catalog-service/src/main/java/com/example/catalogservice/error/dto/ErrorReportRequest.java
@@ -0,0 +1,24 @@
+package com.example.catalogservice.error.dto;
+
+import lombok.Getter;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Getter
+public class ErrorReportRequest {
+    private static final String ERROR_REPORT_FORMAT = "[%s] %s";
+
+    private final HttpServletRequest request;
+    private final Exception exception;
+
+    public ErrorReportRequest(final HttpServletRequest request, final Exception exception) {
+        this.request = request;
+        this.exception = exception;
+    }
+
+    public String getLogMessage() {
+        String requestUri = request.getRequestURI();
+        String requestMethod = request.getMethod();
+        return String.format(ERROR_REPORT_FORMAT, requestMethod, requestUri);
+    }
+}
